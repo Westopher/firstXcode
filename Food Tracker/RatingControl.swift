6d280610519dd32580fcd9b35cf8bfb1cb416edb
West Kraemer <WestKraemer@gmail.com>
2016-10-06 20:30:19 -0400

    Add Coco Touch class for rating system implementation

---
+++ Food Tracker/RatingControl.swift
@@ -0,0 +1,15 @@
+//
+//  RatingControl.swift
+//  Food Tracker
+//
+//  Created by West Kraemer on 10/6/16.
+//  Copyright © 2016 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+
+
+}
