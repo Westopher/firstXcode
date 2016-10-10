e1cc91ed86a4ea0313a1ad11fe1f8cdc2f7a3699
West Kraemer <WestKraemer@gmail.com>
2016-10-10 19:41:32 -0400

    Added rating buttons.

--- Food Tracker/RatingControl.swift
+++ Food Tracker/RatingControl.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class RatingControl: UIView {
 
+// MARK: Properties
+    
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
+    
 // MARK: Initialization
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    
+    for _ in 0..<5 {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         button.backgroundColor = UIColor.red
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), for: .touchDown)
+        ratingButtons += [button]
         addSubview(button)
     }
-
+}
     override var intrinsicContentSize : CGSize {
         return CGSize(width: 240, height: 44)
     }
