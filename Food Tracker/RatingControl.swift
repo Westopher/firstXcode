1d06827e99b220230d8af97794f1987cfc70becb
West Kraemer <WestKraemer@gmail.com>
2016-10-10 15:34:59 -0400

    Update Swift and xCode to latest versions.

--- Food Tracker/RatingControl.swift
+++ Food Tracker/RatingControl.swift
@@ -11,15 +11,22 @@ import UIKit
 class RatingControl: UIView {
 
 // MARK: Initialization
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.redColor()
+        button.backgroundColor = UIColor.red
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), for: .touchDown)
         addSubview(button)
     }
 
-    override func intrinsicContentSize() -> CGSize {
+    override var intrinsicContentSize : CGSize {
         return CGSize(width: 240, height: 44)
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(_ button: UIButton) {
+        print("Button pressed 👍")
     }
     
 }
