138d7dbcd894d083cb320d6ff988abf7d25f90f9
West Kraemer <WestKraemer@gmail.com>
2016-10-10 20:31:55 -0400

    intrensicContentSize() method is now a variable in the new version of Swift. Looking for work arounds. My buttons are still all mashed together as one big button. They never separated.

--- Food Tracker/RatingControl.swift
+++ Food Tracker/RatingControl.swift
@@ -14,6 +14,8 @@ class RatingControl: UIView {
     
     var rating = 0
     var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
     
     
 // MARK: Initialization
@@ -21,24 +23,29 @@ class RatingControl: UIView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     
-    for _ in 0..<5 {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+    for _ in 0..<starCount {
+        let button = UIButton()
         button.backgroundColor = UIColor.red
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), for: .touchDown)
         ratingButtons += [button]
         addSubview(button)
     }
 }
-    override var intrinsicContentSize : CGSize {
-        return CGSize(width: 240, height: 44)
+    override public var intrinsicContentSize: CGSize {
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
     }
     
     override func layoutSubviews() {
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         
         // Offset each button's origin by the length of the button plus spacing.
         for (index, button) in ratingButtons.enumerated() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
             button.frame = buttonFrame
         }
     }
